7c7a663672c17f2f06691e8a70c490badaab1808
9u4a <99junsung@gmail.com>
2022-05-10 23:46:16 +0900

    Update 오븐 시계.py

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,8 +1,11 @@
-a, b = map(int, input().split())
-c = int(input())
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+c = int(sys.stdin.readline().rstrip())
 
 a += c // 60
 b += c % 60
+
 
 if b >= 60:
     a += 1
@@ -11,4 +14,3 @@ if a >= 24:
     a -= 24
 
 print(a, b)
-
