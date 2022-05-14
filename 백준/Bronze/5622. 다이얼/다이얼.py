92cb6a713ce49e734e8dd31837071d44ec4f0ff0
9u4a <99junsung@gmail.com>
2022-05-14 17:08:05 +0900

    [Bronze II] Title: 다이얼, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,24 @@
+import sys
+
+a = sys.stdin.readline().rstrip()
+a = list(a)
+count = 0
+
+for i in a:
+    if i in 'ABC':
+        count += 3
+    elif i in 'DEF':
+        count += 4
+    elif i in 'GHI':
+        count += 5
+    elif i in 'JKL':
+        count += 6
+    elif i in 'MNO':
+        count += 7
+    elif i in 'PQRS':
+        count += 8
+    elif i in 'TUV':
+        count += 9
+    elif i in 'WXYZ':
+        count += 10
+print(count)
